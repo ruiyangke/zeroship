2a0d3a297121c768d4d0507e933c33c3ee0174ab
ruiyangke <me@ry.ke>
2026-08-27 23:35:14 -0700

    test(postgres): compare binary type round trips with tokio

--- libs/compio-postgres/tests/suite/main.rs
+++ libs/compio-postgres/tests/suite/main.rs
@@ -70,6 +70,7 @@ mod copy_out_abandonment;
 mod copy_out_copy_in_resync;
 mod copy_refusal;
 mod differential_tokio;
+mod differential_type_values;
 mod domain_parameters;
 mod error_fields;
 mod execute_row_counts;
